ee22cb37acd5b008dd02a4af676b1e9eaf0cf61b
AhmedhMohamed93 <54803368+AhmedhMohamed93@users.noreply.github.com>
2020-02-24 11:22:00 +0200

    CSharp7Features

---
+++ CSharp7Features/ConsoleApp16/Expression bodied constructor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Expression_bodied_constructor
+    {
+        public int ProductId
+        {get;} = 1;
+        public string ProductName
+        {
+            get;
+        }
+        public decimal Price => 3000;
+        public Expression_bodied_constructor() => ProductName = "Microsoft HoloLens";
+    }
+}
---
+++ CSharp7Features/ConsoleApp16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating binary literals  
+            // by prefixing with 0b 
+            var num1 = 0b1001;
+            var num2 = 0b01000011;
+
+            Console.WriteLine("Value of Num1 is: " + num1);
+            Console.WriteLine("Value of Num2 is: " + num2);
+            Console.WriteLine("Char value of Num2 is: {0}",
+                                     Convert.ToChar(num2));
+            Thread.Sleep(2000);
+
+            // Expression bodied destructor
+            Expression_bodied_constructor Exp = new Expression_bodied_constructor();
+
+
+        }
+    }
+}
